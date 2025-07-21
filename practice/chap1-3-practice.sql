b5e5513a3d72baba39dc913d080e7e11b75d450f
Sirosho <pack8447@gmail.com>
2025-07-21 16:35:30 +0900

    Oracle SQL 연습문제 chap1-3 추가

---
+++ practice/chap1-3-practice.sql
@@ -0,0 +1,36 @@
+
+select *
+from users
+where username = 'pikachu'
+;
+
+select *
+from posts
+where CREATION_DATE between to_date('2023-01-01','YYYY-MM-DD')
+                    and to_date('2023-12-31','YYYY-MM-DD')
+;
+select username,email
+from users
+where user_id in (1,2,3,4,5,6,7,8)
+;
+
+select post_id,content
+from posts
+where content like '%#먹스타그램%'
+;
+
+select *
+from posts
+where user_id = 1 and post_type = 'photo'
+;
+
+select username
+from users
+where user_id not between(1)
+            and(20)
+
+
+
+
+
+
