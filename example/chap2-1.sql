aaa3a2eed7a3f341f85ed7eb478a85a536f2df7a
Sirosho <pack8447@gmail.com>
2025-07-28 15:58:32 +0900

    CROSS, NATURAL JOIN , USING
    UNION

--- example/chap2-1.sql
+++ example/chap2-1.sql
@@ -12,3 +12,18 @@ SELECT USER_ID FROM LIKES
 UNION
 SELECT USER_ID  FROM COMMENTS
 ;
+
+
+
+-- '좋아요'를 누른 사용자의 ID 목록 (중복 제거됨)
+SELECT user_id FROM LIKES
+INTERSECT
+-- '댓글'을 작성한 사용자의 ID 목록 (중복 제거됨)
+SELECT user_id FROM COMMENTS;
+
+
+-- '좋아요'를 누른 사용자의 ID 목록
+SELECT user_id FROM LIKES
+MINUS
+-- '댓글'을 작성한 사용자의 ID 목록
+SELECT user_id FROM COMMENTS;
